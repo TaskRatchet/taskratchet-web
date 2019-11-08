5a886c725b664b4e293019d7834c99c99738d9b2
Nathan Arthur <nathan@nathanarthur.com>
2019-11-08 15:33:06 -0500

    Convert login & reigster to page components; add succes & fail registration messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React, {FormEvent} from 'react';
 import './App.css';
-import LoginForm from './components/molecules/Login'
-import RegisterForm from './components/molecules/Register';
+import LoginForm from './components/pages/Login'
+import RegisterForm from './components/pages/Register';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 type User = {
     email: string
@@ -12,254 +15,28 @@ interface AppProps {
 
 interface AppState {
     user: User | null,
-    messages: string[],
-    loginForm: {
-        email: string,
-        password: string
-    },
-    registerForm: {
-        name: string,
-        email: string,
-        password: string,
-        password2: string,
-        timezones: string[],
-        timezone: string,
-    }
 }
 
 class App extends React.Component<AppProps, {}> {
     state: AppState = {
         user: null,
-        messages: [],
-        loginForm: {
-            email: '',
-            password: ''
-        },
-        registerForm: {
-            name: '',
-            email: '',
-            password: '',
-            password2: '',
-            timezones: [],
-            timezone: '',
-        }
     };
 
     componentDidMount(): void {
         document.title = 'TaskRatchet';
-        this.populateTimezones();
+
+        console.log(cookies.get('tr_session'));
     }
-
-    populateTimezones = () => {
-        fetch('https://us-central1-taskratchet.cloudfunctions.net/api1/timezones')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState((prev: AppState) => {
-                    prev.registerForm.timezones = data;
-                    prev.registerForm.timezone = data[0];
-                    return prev;
-                });
-            });
-    };
-
-    login = (event: any) => {
-        event.preventDefault();
-
-        console.log('logging in', this.state.loginForm);
-
-        this.clearMessages();
-        const passes = this.validateLoginForm();
-
-        if (!passes) return;
-
-        fetch('https://us-central1-taskratchet.cloudfunctions.net/api1/account/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                'email': this.state.loginForm.email,
-                'password': this.state.loginForm.password,
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => {
-            console.log(res);
-            if (res.status === 403) {
-                this.pushMessage('Login failed');
-            } else {
-                this.pushMessage('Login successful');
-            }
-        });
-    };
-
-    validateLoginForm = () => {
-        let passes = true;
-
-        if (!this.state.loginForm.email) {
-            this.pushMessage('Email required')
-            passes = false;
-        }
-
-        if (!this.state.loginForm.password) {
-            this.pushMessage('Password required')
-            passes = false;
-        }
-
-        return passes;
-    };
-
-    setLoginEmail = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.loginForm.email = t.value;
-            return prev;
-        })
-    };
-
-    setLoginPassword = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.loginForm.password = t.value;
-            return prev;
-        })
-    };
-
-    setRegisterName = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.registerForm.name = t.value;
-            return prev;
-        })
-    };
-
-    setRegisterEmail = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.registerForm.email = t.value;
-            return prev;
-        })
-    };
-
-    setRegisterPassword = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.registerForm.password = t.value;
-            return prev;
-        })
-    };
-
-    setRegisterPassword2 = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.registerForm.password2 = t.value;
-            return prev;
-        })
-    };
-
-    setRegisterTimezone = (event: any) => {
-        const t = event.target;
-        this.setState((prev: AppState) => {
-            prev.registerForm.timezone = t.value;
-            return prev;
-        })
-    };
-
-    register = (event: any) => {
-        event.preventDefault();
-
-        console.log('registering', this.state.registerForm);
-
-        this.clearMessages();
-        const passes = this.validateRegistrationForm();
-
-        if (!passes) return;
-
-        console.log('posting registration');
-
-        fetch('https://us-central1-taskratchet.cloudfunctions.net/api1/account/register', {
-            method: 'POST',
-            body: JSON.stringify({
-                'name': this.state.registerForm.name,
-                'email': this.state.registerForm.email,
-                'password': this.state.registerForm.password,
-                'timezone': this.state.registerForm.timezone
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json()).then(res => console.log(res));
-    };
-
-    clearMessages = () => {
-        this.setState((prev: AppState) => {
-            prev.messages = [];
-            return prev;
-        });
-    };
-
-    validateRegistrationForm = () => {
-        let passes = true;
-
-        if (!this.state.registerForm.email) {
-            this.pushMessage("Email missing");
-            passes = false;
-        }
-
-        if (!this.state.registerForm.password || !this.state.registerForm.password2) {
-            this.pushMessage("Please enter password twice");
-            passes = false;
-        }
-
-        if (this.state.registerForm.password !== this.state.registerForm.password2) {
-            this.pushMessage("Passwords don't match");
-            passes = false;
-        }
-
-        return passes;
-    };
-
-    pushMessage = (msg: string) => {
-        this.setState((prev: AppState) => {
-            prev.messages.push(msg);
-            return prev;
-        });
-    };
-
-    logout = () => {
-        this.setState((prev: AppState) => {
-            prev.user = null;
-            return prev;
-        });
-    };
 
     render() {
         return <div>
-            {this.state.messages.map((msg, i) => <p key={i}>{msg}</p>)}
-
             <h1>Login</h1>
-            <LoginForm
-                isLoggedIn={!!this.state.user}
-                email={this.state.loginForm.email}
-                password={this.state.loginForm.password}
-                onEmailChange={this.setLoginEmail}
-                onPasswordChange={this.setLoginPassword}
-                onSubmit={this.login}
-            />
+            <LoginForm />
 
             <h1>Register</h1>
-            <RegisterForm
-                name={this.state.registerForm.name}
-                email={this.state.registerForm.email}
-                password={this.state.registerForm.password}
-                password2={this.state.registerForm.password2}
-                timezones={this.state.registerForm.timezones}
-                timezone={this.state.registerForm.timezone}
-                onNameChange={this.setRegisterName}
-                onEmailChange={this.setRegisterEmail}
-                onPasswordChange={this.setRegisterPassword}
-                onPassword2Change={this.setRegisterPassword2}
-                onTimezoneChange={this.setRegisterTimezone}
-                onSubmit={this.register}
-            />
+            <RegisterForm />
+
+            <h1>Tasks</h1>
         </div>
     }
 }
---
+++ src/components/pages/Login/index.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+interface LoginState {
+    messages: string[],
+    email: string,
+    password: string
+}
+
+class Login extends React.Component<{}, LoginState> {
+    state: LoginState = {
+        messages: [],
+        email: '',
+        password: ''
+    };
+
+    login = (event: any) => {
+        event.preventDefault();
+
+        console.log('logging in', this.state);
+
+        this.clearMessages();
+        const passes = this.validateLoginForm();
+
+        if (!passes) return;
+
+        fetch('https://us-central1-taskratchet.cloudfunctions.net/api1/account/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                'email': this.state.email,
+                'password': this.state.password,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res: any) => {
+                if (res.status === 403) {
+                    this.pushMessage('Login failed');
+                } else {
+                    this.pushMessage('Login successful');
+                    return res.text();
+                }
+            })
+            .then((res: any) => {
+                console.log(res);
+                cookies.set('tr_session', res);
+            });
+    };
+
+    validateLoginForm = () => {
+        let passes = true;
+
+        if (!this.state.email) {
+            this.pushMessage('Email required');
+            passes = false;
+        }
+
+        if (!this.state.password) {
+            this.pushMessage('Password required');
+            passes = false;
+        }
+
+        return passes;
+    };
+
+    setLoginEmail = (event: any) => {
+        const t = event.target;
+        this.setState((prev: LoginState) => {
+            prev.email = t.value;
+            return prev;
+        })
+    };
+
+    setLoginPassword = (event: any) => {
+        const t = event.target;
+        this.setState((prev: LoginState) => {
+            prev.password = t.value;
+            return prev;
+        })
+    };
+
+    clearMessages = () => {
+        this.setState((prev: LoginState) => {
+            prev.messages = [];
+            return prev;
+        });
+    };
+
+    pushMessage = (msg: string) => {
+        this.setState((prev: LoginState) => {
+            prev.messages.push(msg);
+            return prev;
+        });
+    };
+
+    logout = () => {
+        // this.setState((prev: LoginState) => {
+        //     prev.user = null;
+        //     return prev;
+        // });
+    };
+
+    render() {
+        return <form onSubmit={this.login}>
+            {this.state.messages.map((msg, i) => <p key={i}>{msg}</p>)}
+
+            <input
+                type="email"
+                value={this.state.email}
+                onChange={this.setLoginEmail}
+                name={'email'}
+                placeholder={'Email'}
+            /><br/>
+
+            <input
+                type="password"
+                value={this.state.password}
+                onChange={this.setLoginPassword}
+                name={'password'}
+                placeholder={'Password'}
+            /><br/>
+
+            <input type="submit" value={'Submit'} />
+        </form>
+    }
+}
+
+export default Login;
