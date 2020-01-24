b2a4d9a6e89b7876ac3f61126c99ce89e507a40b
Nathan Arthur <nathan@nathanarthur.com>
2020-01-24 07:34:49 -0500

    Style task list

--- src/components/molecules/Task/index.tsx
+++ src/components/molecules/Task/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import './style.css'
+import './style.css'
 
 interface TaskProps {
     task: Task,
@@ -8,8 +8,10 @@ interface TaskProps {
 
 const Task = (props: TaskProps) => {
     return <div className={'molecule-task'}>
-        <input type="checkbox" onChange={props.onToggle} checked={props.task.complete}/>&nbsp;
-        {props.task.task} by {props.task.due} or pay ${props.task.stakes}
+        <input type="checkbox" onChange={props.onToggle} checked={props.task.complete}/>
+        <span className="molecule-task__description">{props.task.task || '[Description Missing]'}</span>
+        <span className={'molecule-task__due'}>{props.task.due}</span>
+        <span className={'molecule-task__stakes'}>${props.task.stakes}</span>
     </div>
 };
 
