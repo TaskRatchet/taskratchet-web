c2b8f70a77696ae148e50b207891a36e33a2fd03
Nathan Arthur <nathan@nathanarthur.com>
2020-08-14 05:14:35 -0400

    fix footer link

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css'
 
 const Footer = () => <div className={'organism-footer'}>
-    <a href="https://docs.taskratchet.com" target={"_blank"} rel={"nofollow noopener"}>Help</a>
+    <a href="https://docs.taskratchet.com" target={"_blank"} rel={"noopener noreferrer"}>Help</a>
 </div>
 
 export default Footer
