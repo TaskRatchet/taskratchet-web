7d35076e12e1a37e41c978b6ff93694e25ba540e
Nathan Arthur <nathan@nathanarthur.com>
2020-08-28 17:06:45 -0400

    Refactor tasks page tests

--- src/components/pages/Tasks/Tasks.test.tsx
+++ src/components/pages/Tasks/Tasks.test.tsx
@@ -7,17 +7,40 @@ import React from "react";
 
 jest.mock('../../../classes/Api')
 
+const loadApiData = ({tasks = [], me = {}}: { tasks?: Task[], me?: object }) => {
+    (api.getTasks as jest.Mock).mockReturnValue({json: () => Promise.resolve(tasks || [])});
+    (api.getMe as jest.Mock).mockReturnValue({json: () => Promise.resolve(me || {})});
+}
+
+const makeTask = (
+    {
+        complete = true,
+        due = 0,
+        id = 0,
+        cents = 0,
+        task = 'the_task',
+        charge_locked = null,
+        charge_authorized = null,
+        charge_captured = null
+    } = {}
+): Task => {
+    return {
+        complete,
+        due,
+        id,
+        cents,
+        task,
+        charge_locked,
+        charge_authorized,
+        charge_captured,
+    }
+}
+
 describe("tasks page", () => {
     it("loads tasks", async () => {
-        (api.getTasks as jest.Mock).mockReturnValue({json: () => Promise.resolve([
-                {
-                    task: 'the_task',
-                    id: 3
-                }
-            ])});
-        (api.getMe as jest.Mock).mockReturnValue({json: () => Promise.resolve({})});
+        loadApiData({tasks: [makeTask()]})
 
-        const {getByText} = render(<Tasks />)
+        const {getByText} = render(<Tasks/>)
 
         await waitFor(() => expect(getByText('the_task')).toBeDefined())
     })
