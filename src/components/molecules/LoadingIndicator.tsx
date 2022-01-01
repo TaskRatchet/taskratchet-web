e6c5681bfb14b5821fea9fc43bf2de4b8aeaf86f
Nathan Arthur <nathan@nathanarthur.com>
2022-01-01 16:21:09 -0500

    use mui for progress indicator

--- src/components/molecules/LoadingIndicator.tsx
+++ src/components/molecules/LoadingIndicator.tsx
@@ -1,23 +1,15 @@
-import TopBarProgress from 'react-topbar-progress-indicator';
 import React from 'react';
 import { useIsFetching } from 'react-query';
-
-TopBarProgress.config({
-	barColors: {
-		'0': '#00ceff',
-		'1.0': '#e600ff',
-	},
-	barThickness: 7,
-});
+import { LinearProgress } from '@mui/material';
 
 const LoadingIndicator = (): JSX.Element | null => {
 	const isFetching = useIsFetching();
 
-	return isFetching ? (
-		<span style={{ pointerEvents: 'none' }}>
-			<TopBarProgress />
-		</span>
-	) : null;
+	return (
+		<div style={{ pointerEvents: 'none', height: 5 }}>
+			{isFetching ? <LinearProgress color={'secondary'} /> : null}
+		</div>
+	);
 };
 
 export default LoadingIndicator;
