36563845b566a043d614ae5c7e2e585cc8730d0a
Nathan Arthur <nathan@nathanarthur.com>
2022-03-26 09:53:41 -0400

    refactor Task test suite

--- src/components/molecules/Task.spec.tsx
+++ src/components/molecules/Task.spec.tsx
@@ -1,11 +1,15 @@
 import Task from './Task';
 import React from 'react';
-import { renderWithQueryProvider } from '../../lib/test/helpers';
+import {
+	renderWithQueryProvider,
+	withMutedReactQueryLogger,
+} from '../../lib/test/helpers';
 import userEvent from '@testing-library/user-event';
 import { updateTask } from '../../lib/api';
 import { waitFor } from '@testing-library/dom';
 import browser from '../../lib/Browser';
 import { editTask } from '../../lib/api/editTask';
+import { screen } from '@testing-library/react';
 
 jest.mock('../../lib/api/updateTask');
 jest.mock('date-fns');
@@ -15,26 +19,38 @@ jest.mock('../../lib/api/editTask');
 
 const mockEditTask = editTask as jest.Mock;
 
+function renderTask(task: Partial<TaskType> = {}) {
+	const t: TaskType = {
+		due: '2/1/2022, 11:59 PM',
+		cents: 100,
+		task: 'the_task',
+		id: 'the_id',
+		status: 'pending',
+		complete: false,
+		timezone: 'Est/GMT',
+		...task,
+	};
+	return renderWithQueryProvider(<Task task={t} />);
+}
+
+async function openEditDialog() {
+	userEvent.click(screen.getByLabelText('Menu'));
+	userEvent.click(screen.getByText('Edit'));
+
+	await waitFor(() => {
+		expect(screen.getByLabelText('Due Date *')).toHaveValue('02/01/2022');
+	});
+}
+
 describe('Task component', () => {
 	beforeEach(() => {
 		jest.spyOn(browser, 'scrollIntoView').mockImplementation(() => undefined);
 	});
 
 	it('disables checkbox for tasks without id', async () => {
-		const { container } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					status: 'pending',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask({ id: undefined });
 
-		const checkbox = container.querySelector('input') as HTMLInputElement;
+		const checkbox = screen.getByRole('checkbox') as HTMLInputElement;
 
 		userEvent.click(checkbox);
 
@@ -42,65 +58,29 @@ describe('Task component', () => {
 	});
 
 	it('has task menu', async () => {
-		const { getByLabelText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask();
 
-		expect(getByLabelText('Menu')).toBeInTheDocument();
+		expect(screen.getByLabelText('Menu')).toBeInTheDocument();
 	});
 
 	it('has uncle menu item', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask();
 
-		const menuButton = getByLabelText('Menu');
+		const menuButton = screen.getByLabelText('Menu');
 
 		userEvent.click(menuButton);
 
-		expect(getByText('Charge immediately')).toBeInTheDocument();
+		expect(screen.getByText('Charge immediately')).toBeInTheDocument();
 	});
 
 	it('uncles', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask();
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Charge immediately'));
+		userEvent.click(screen.getByLabelText('Menu'));
+		userEvent.click(screen.getByText('Charge immediately'));
 
 		await waitFor(() => {
-			userEvent.click(getByText('Charge'));
+			userEvent.click(screen.getByText('Charge'));
 		});
 
 		await waitFor(() =>
@@ -111,90 +91,39 @@ describe('Task component', () => {
 	});
 
 	it('disables entry for expired tasks', async () => {
-		const { getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'expired',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask({ status: 'expired' });
 
-		const desc = getByText('the_task') as HTMLElement;
+		const desc = screen.getByText('the_task') as HTMLElement;
 		const item = desc.closest('.MuiListItem-root') as HTMLElement;
 
 		expect(item.querySelector('input')).not.toBeInTheDocument();
 	});
 
 	it('replaces checkbox with icon for expired tasks', async () => {
-		const { container } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'expired',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask({ status: 'expired' });
 
-		expect(container.querySelector('input')).not.toBeInTheDocument();
+		expect(screen.queryByRole('checkbox')).not.toBeInTheDocument();
 	});
 
 	it('disables uncle button if task not pending', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: 'the_due',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'expired',
-					timezone: 'Etc/GMT',
-					complete: false,
-				}}
-			/>
-		);
+		renderTask({ status: 'expired' });
 
-		const menuButton = getByLabelText('Menu');
+		const menuButton = screen.getByLabelText('Menu');
 
 		userEvent.click(menuButton);
 
-		expect(getByText('Charge immediately')).toHaveAttribute('aria-disabled');
+		expect(screen.getByText('Charge immediately')).toHaveAttribute(
+			'aria-disabled'
+		);
 	});
 
 	it('allows cents edit', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					complete: false,
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					timezone: 'Etc/GMT',
-				}}
-			/>
-		);
+		renderTask({ cents: 100 });
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Edit'));
+		await openEditDialog();
 
-		await waitFor(() => {
-			getByLabelText('Stakes *');
-		});
-
-		userEvent.type(getByLabelText('Stakes *'), '5');
-		userEvent.click(getByText('Save'));
+		userEvent.type(screen.getByLabelText('Stakes *'), '5');
+		userEvent.click(screen.getByText('Save'));
 
 		await waitFor(() => {
 			expect(editTask).toBeCalledWith('the_id', '2/1/2022, 11:59 PM', 1500);
@@ -202,56 +131,25 @@ describe('Task component', () => {
 	});
 
 	it('closes task menu on edit click', async () => {
-		const { getByLabelText, getByText, queryByAltText } =
-			await renderWithQueryProvider(
-				<Task
-					task={{
-						due: '2/1/2022, 11:59 PM',
-						cents: 100,
-						task: 'the_task',
-						id: 'the_id',
-						status: 'pending',
-						complete: false,
-						timezone: 'Est/GMT',
-					}}
-				/>
-			);
+		renderTask();
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Edit'));
+		userEvent.click(screen.getByLabelText('Menu'));
+		userEvent.click(screen.getByText('Edit'));
 
-		expect(queryByAltText('Edit')).not.toBeInTheDocument();
+		expect(screen.queryByAltText('Edit')).not.toBeInTheDocument();
 	});
 
 	it('allows date edit', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					complete: false,
-					timezone: 'Est/GMT',
-				}}
-			/>
+		renderTask({ due: '2/1/2022, 11:59 PM' });
+
+		await openEditDialog();
+
+		await userEvent.type(
+			screen.getByLabelText('Due Date *'),
+			'{backspace}5{enter}'
 		);
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Edit'));
-
-		await waitFor(() => {
-			expect(getByLabelText('Due Date *')).toHaveValue('02/01/2022');
-		});
-
-		userEvent.type(getByLabelText('Due Date *'), '{backspace}5{enter}');
-
-		await waitFor(() => {
-			expect(getByLabelText('Due Date *')).toHaveValue('02/01/2025');
-		});
-
-		userEvent.click(getByText('Save'));
+		userEvent.click(screen.getByText('Save'));
 
 		await waitFor(() => {
 			expect(editTask).toBeCalledWith('the_id', '2/1/2025, 11:59 PM', 100);
@@ -259,23 +157,11 @@ describe('Task component', () => {
 	});
 
 	it('disables edit if task is not pending', async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'complete',
-					complete: true,
-					timezone: 'Est/GMT',
-				}}
-			/>
-		);
+		renderTask({ status: 'expired' });
 
-		userEvent.click(getByLabelText('Menu'));
+		userEvent.click(screen.getByLabelText('Menu'));
 
-		expect(getByText('Edit')).toHaveAttribute('aria-disabled');
+		expect(screen.getByText('Edit')).toHaveAttribute('aria-disabled');
 	});
 
 	it('asks for confirmation before uncling', async () => {
@@ -284,95 +170,58 @@ describe('Task component', () => {
 		should be shown confirmation step before the task is charged. 
 		 */
 
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					complete: false,
-					timezone: 'Est/GMT',
-				}}
-			/>
-		);
+		renderTask();
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Charge immediately'));
+		userEvent.click(screen.getByLabelText('Menu'));
+		userEvent.click(screen.getByText('Charge immediately'));
 
 		// displays confirmation dialog
 		await waitFor(() => {
-			expect(getByText('Charge')).toBeInTheDocument();
+			expect(screen.getByText('Charge')).toBeInTheDocument();
 		});
 
 		expect(updateTask).not.toBeCalled();
 	});
 
 	it("tells you know how much you'll be charged if you uncle", async () => {
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					complete: false,
-					timezone: 'Est/GMT',
-				}}
-			/>
-		);
+		renderTask({ cents: 100 });
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Charge immediately'));
+		userEvent.click(screen.getByLabelText('Menu'));
+		userEvent.click(screen.getByText('Charge immediately'));
 
 		await waitFor(() => {
 			expect(
-				getByText('If you confirm, you will immediately be charged $1.', {
-					exact: false,
-				})
+				screen.getByText(
+					'If you confirm, you will immediately be charged $1.',
+					{
+						exact: false,
+					}
+				)
 			).toBeInTheDocument();
 		});
 	});
 
 	it('displays edit errors from API', async () => {
-		mockEditTask.mockRejectedValue({ message: 'the_error' });
+		await withMutedReactQueryLogger(async () => {
+			mockEditTask.mockRejectedValue({ message: 'the_error' });
 
-		const { getByLabelText, getByText } = await renderWithQueryProvider(
-			<Task
-				task={{
-					due: '2/1/2022, 11:59 PM',
-					cents: 100,
-					task: 'the_task',
-					id: 'the_id',
-					status: 'pending',
-					complete: false,
-					timezone: 'Est/GMT',
-				}}
-			/>
-		);
+			renderTask();
 
-		userEvent.click(getByLabelText('Menu'));
-		userEvent.click(getByText('Edit'));
+			await openEditDialog();
 
-		await waitFor(() => {
-			expect(getByLabelText('Due Date *')).toHaveValue('02/01/2022');
+			await userEvent.type(
+				screen.getByLabelText('Due Date *'),
+				'{backspace}5{enter}'
+			);
+
+			userEvent.click(screen.getByText('Save'));
+
+			await waitFor(() => {
+				expect(editTask).toBeCalledWith('the_id', '2/1/2025, 11:59 PM', 100);
+			});
+
+			expect(screen.getByText('the_error')).toBeInTheDocument();
 		});
-
-		userEvent.type(getByLabelText('Due Date *'), '{backspace}5{enter}');
-
-		await waitFor(() => {
-			expect(getByLabelText('Due Date *')).toHaveValue('02/01/2025');
-		});
-
-		userEvent.click(getByText('Save'));
-
-		await waitFor(() => {
-			expect(editTask).toBeCalledWith('the_id', '2/1/2025, 11:59 PM', 100);
-		});
-
-		expect(getByText('the_error')).toBeInTheDocument();
 	});
 
 	// TODO: triggers task list reload
