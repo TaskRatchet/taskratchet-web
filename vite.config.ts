432af407e95ea84cc03375d3d3565032027fb18d
Nathan Arthur <nathan@nathanarthur.com>
2022-08-23 17:05:10 -0400

    add bundle visualize command

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), visualizer()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
