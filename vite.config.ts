cf3fc9d707e7cf78a11707320b3b83822066f746
Nathan Arthur <nathan@nathanarthur.com>
2022-08-24 15:22:59 -0400

    compress, don't gzip
    netlify already gzips at runtime

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import viteCompression from 'vite-plugin-compression';
+import compress from 'vite-plugin-compress';
 
 export default defineConfig({
-	plugins: [
-		react(),
-		visualizer(),
-		viteCompression({
-			deleteOriginFile: true,
-		}),
-	],
+	plugins: [react(), visualizer(), compress()],
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./global-setup.ts'],
