7421ef2f3da2ca4358e4ca45702c970f39d1d2f7
Nathan Arthur <nathan@nathanarthur.com>
2022-08-24 15:14:07 -0400

    enable gzipping

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-	plugins: [react(), visualizer()],
+	plugins: [
+		react(),
+		visualizer(),
+		viteCompression({
+			deleteOriginFile: true,
+		}),
+	],
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./global-setup.ts'],
