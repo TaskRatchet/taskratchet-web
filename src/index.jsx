10c7359e906aa8fdee9e755da5737048faca0183
Nathan Arthur <nathan@nathanarthur.com>
2022-10-21 14:45:01 -0400

    formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import App from './App';
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App lakes={[]} />)
+root.render(<App lakes={[]} />);
