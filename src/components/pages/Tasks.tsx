9f913281fd7d2690b507950ed0189911ff48dd01
Nathan Arthur <nathan@nathanarthur.com>
2022-11-08 16:49:36 -0500

    remove unused props

--- src/components/pages/Tasks.tsx
+++ src/components/pages/Tasks.tsx
@@ -4,18 +4,14 @@ import TaskList from '../organisms/TaskList';
 import { useCloseWarning } from '../../lib/useCloseWarning';
 import { Box } from '@mui/material';
 
-interface TasksProps {
-	lastToday: Date | undefined;
-}
-
-const Tasks = ({ lastToday }: TasksProps): JSX.Element => {
+const Tasks = (): JSX.Element => {
 	const [newTask, setNewTask] = useState<TaskType>();
 
 	useCloseWarning();
 
 	return (
 		<Box sx={{ pb: 12 }}>
-			<TaskList lastToday={lastToday} newTask={newTask} />
+			<TaskList newTask={newTask} />
 			<PlusFab onSave={setNewTask} />
 		</Box>
 	);
