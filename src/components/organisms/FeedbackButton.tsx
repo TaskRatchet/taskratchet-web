fea934fb9530b81bf099d44678c5404385ebb4c4
Nathan Arthur <nathan@nathanarthur.com>
2022-11-23 12:30:07 -0500

    use freshdesk widget

--- src/components/organisms/FeedbackButton.tsx
+++ src/components/organisms/FeedbackButton.tsx
@@ -1,47 +1,33 @@
-import {
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-	IconButton,
-	Tooltip,
-	TextField,
-	Alert,
-	Stack,
-} from '@mui/material';
-import React, { useState, useEffect } from 'react';
+import { IconButton, Tooltip } from '@mui/material';
+import React, { useEffect } from 'react';
 import FeedbackIcon from '@mui/icons-material/Feedback';
-import createTicket from '../../lib/createTicket';
 import { useMe } from '../../lib/api/useMe';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { useMutation } from 'react-query';
 
 export default function FeedbackButton() {
-	const { data } = useMe();
-
-	const [open, setOpen] = useState(false);
-	const [email, setEmail] = useState(data?.email);
-	const [message, setMessage] = useState('');
-	const [validate, setValidate] = useState(false);
-
-	useEffect(() => {
-		if (email === undefined && data) {
-			setEmail(data.email);
-		}
-	}, [data, email]);
+	useMe({
+		onSuccess: (data) => {
+			console.log('identifying', data);
+			window.FreshworksWidget('identify', 'ticketForm', {
+				name: data.name,
+				email: data.email,
+			});
+			window.FreshworksWidget('prefill', 'ticketForm', {
+				custom_fields: data,
+			});
+		},
+	});
 
 	useEffect(() => {
 		window.FreshworksWidget('hide', 'launcher');
 	}, []);
 
-	const mutation = useMutation(createTicket);
-
 	return (
 		<>
 			<Tooltip title={'Feedback'}>
 				<IconButton
-					onClick={() => setOpen(true)}
+					onClick={() => {
+						window.FreshworksWidget('open');
+					}}
 					edge="start"
 					color="inherit"
 					aria-label="feedback"
@@ -50,78 +36,6 @@ export default function FeedbackButton() {
 					<FeedbackIcon />
 				</IconButton>
 			</Tooltip>
-			<Dialog open={open} onClose={() => setOpen(false)}>
-				<DialogTitle>Feedback</DialogTitle>
-				<DialogContent dividers>
-					<Stack spacing={2}>
-						{mutation.isSuccess && (
-							<Alert severity="success">Thank you for your feedback!</Alert>
-						)}
-
-						{mutation.isError && (
-							<Alert severity="error">
-								Oops! Something went wrong. Please send an email to
-								support@taskratchet.com and include the following error:
-								<br />
-								<br />
-								{mutation.error instanceof Error
-									? mutation.error.message
-									: JSON.stringify(mutation.error)}
-							</Alert>
-						)}
-
-						<TextField
-							label="Email"
-							type="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							required
-							fullWidth
-							error={validate && !email}
-							helperText={validate && !email && 'Required'}
-						/>
-
-						<TextField
-							label="Message"
-							autoFocus
-							multiline
-							value={message}
-							onChange={(e) => {
-								setMessage(e.target.value);
-							}}
-							required
-							fullWidth
-							error={validate && !message}
-							helperText={validate && !message && 'Required'}
-						/>
-					</Stack>
-				</DialogContent>
-				<DialogActions>
-					<Button
-						onClick={() => {
-							setOpen(false);
-						}}
-					>
-						Cancel
-					</Button>
-					<LoadingButton
-						loading={mutation.isLoading}
-						variant="contained"
-						onClick={() => {
-							setValidate(true);
-							if (email && message) {
-								mutation.mutate({
-									description: message,
-									unique_external_id: data?.id,
-									email,
-								});
-							}
-						}}
-					>
-						Submit
-					</LoadingButton>
-				</DialogActions>
-			</Dialog>
 		</>
 	);
 }
--- src/components/organisms/NavBar.spec.tsx
+++ src/components/organisms/NavBar.spec.tsx
@@ -4,12 +4,8 @@ import { useSession } from '../../lib/api/useSession';
 import userEvent from '@testing-library/user-event';
 import { screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import { vi, Mock, expect, it, describe, beforeEach } from 'vitest';
+import { vi, Mock, expect, it, describe } from 'vitest';
 import { renderWithQueryProvider } from '../../lib/test/renderWithQueryProvider';
-import createTicket from '../../lib/createTicket';
-import { getMe } from '../../lib/api/getMe';
-import { withMutedReactQueryLogger } from '../../lib/test/withMutedReactQueryLogger';
-import loadControlledPromise from '../../lib/test/loadControlledPromise';
 
 vi.mock('../../lib/api/useSession');
 
@@ -21,23 +17,8 @@ function renderComponent() {
 	);
 }
 
-async function fillForm() {
-	await userEvent.type(await screen.findByLabelText(/Email/), 'the_email');
-	await userEvent.type(await screen.findByLabelText(/Message/), 'the_message');
-}
-
 describe('NavBar', () => {
 	const mockUseSession = useSession as Mock;
-
-	beforeEach(() => {
-		vi.mocked(getMe).mockClear();
-		vi.mocked(createTicket).mockClear();
-		vi.mocked(getMe).mockResolvedValue({
-			data: {
-				id: 'the_user_id',
-			},
-		} as any);
-	});
 
 	it('displays email', async () => {
 		mockUseSession.mockReturnValue({
@@ -119,187 +100,6 @@ describe('NavBar', () => {
 
 		await waitFor(() => {
 			expect(screen.queryByText('Logout')).not.toBeInTheDocument();
-		});
-	});
-
-	it('opens feedback form', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-
-		expect(screen.getByText('Feedback')).toBeInTheDocument();
-	});
-
-	it('submits feedback form', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await fillForm();
-		await userEvent.click(await screen.findByText('Submit'));
-
-		expect(createTicket).toHaveBeenCalled();
-	});
-
-	it('sends feedback in ticket', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await fillForm();
-		await userEvent.click(await screen.findByText('Submit'));
-
-		expect(createTicket).toHaveBeenCalledWith(
-			expect.objectContaining({
-				description: 'the_message',
-			})
-		);
-	});
-
-	it('includes user id in ticket', async () => {
-		vi.mocked(getMe).mockResolvedValue({
-			id: 'the_user_id',
-		} as any);
-
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await fillForm();
-		await userEvent.click(await screen.findByText('Submit'));
-
-		expect(createTicket).toHaveBeenCalledWith(
-			expect.objectContaining({
-				unique_external_id: 'the_user_id',
-			})
-		);
-	});
-
-	it('asks for email address if user not logged in', async () => {
-		await withMutedReactQueryLogger(async () => {
-			vi.mocked(getMe).mockRejectedValue(new Error('Not logged in'));
-
-			renderComponent();
-
-			await userEvent.click(await screen.findByLabelText('feedback'));
-			await userEvent.type(await screen.findByLabelText(/Email/), 'the_email');
-			await userEvent.type(
-				await screen.findByLabelText(/Message/),
-				'the_message'
-			);
-			await userEvent.click(await screen.findByText('Submit'));
-
-			expect(createTicket).toHaveBeenCalledWith(
-				expect.objectContaining({
-					email: 'the_email',
-				})
-			);
-		});
-	});
-
-	it('uses loading button', async () => {
-		const { resolve } = loadControlledPromise(createTicket);
-
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await fillForm();
-		await userEvent.click(await screen.findByText('Submit'));
-
-		await screen.findByRole('progressbar');
-
-		resolve();
-	});
-
-	it('shows success message', async () => {
-		const { resolve } = loadControlledPromise(createTicket);
-
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await fillForm();
-		await userEvent.click(await screen.findByText('Submit'));
-
-		await screen.findByRole('progressbar');
-
-		resolve();
-
-		await screen.findByText('Thank you for your feedback!');
-	});
-
-	it('shows error message', async () => {
-		await withMutedReactQueryLogger(async () => {
-			const { reject } = loadControlledPromise(createTicket);
-
-			renderComponent();
-
-			await userEvent.click(await screen.findByLabelText('feedback'));
-			await fillForm();
-			await userEvent.click(await screen.findByText('Submit'));
-
-			await screen.findByRole('progressbar');
-
-			reject(new Error('the error'));
-
-			await screen.findByText(/the error/);
-		});
-	});
-
-	it('validates fields', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await userEvent.click(await screen.findByText('Submit'));
-
-		await screen.findAllByText('Required');
-	});
-
-	it('waits for submit click before showing validation errors', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-
-		expect(screen.queryAllByText('Required')).toHaveLength(0);
-	});
-
-	it('does not submit feedback if validation fails', async () => {
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await userEvent.click(await screen.findByText('Submit'));
-
-		expect(createTicket).not.toHaveBeenCalled();
-	});
-
-	it('loads email on user login', async () => {
-		vi.mocked(getMe).mockResolvedValue(undefined as any);
-
-		const { queryClient } = renderComponent();
-
-		vi.mocked(getMe).mockResolvedValue({
-			id: 'the_user_id',
-			email: 'the_email',
-		} as any);
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-
-		await queryClient.invalidateQueries('me');
-
-		await waitFor(() => {
-			expect(screen.getByLabelText(/Email/)).toHaveValue('the_email');
-		});
-	});
-
-	it('does not refill email when user clears field', async () => {
-		vi.mocked(getMe).mockResolvedValue({
-			id: 'the_user_id',
-			email: 'the_email',
-		} as any);
-
-		renderComponent();
-
-		await userEvent.click(await screen.findByLabelText('feedback'));
-		await userEvent.clear(await screen.findByLabelText(/Email/));
-
-		await waitFor(() => {
-			expect(screen.getByLabelText(/Email/)).toHaveValue('');
 		});
 	});
 });
