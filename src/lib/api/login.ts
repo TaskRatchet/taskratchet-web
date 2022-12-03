82b685bff66308b3be504f74771eaee8d3f0eca4
Nathan Arthur <nathan@nathanarthur.com>
2022-12-02 21:13:45 -0500

    wip

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,17 +1,57 @@
 import { publishSession } from './useSession';
 import fetch1 from './fetch1';
+<<<<<<< Updated upstream
+=======
+import logEvent from '../logEvent';
+import { EventCategory, EventAction } from '../logEvent';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+>>>>>>> Stashed changes
 
-export function login(email: string, password: string): Promise<boolean> {
-	return fetch1('account/login', false, 'POST', {
-		email: email,
-		password: password,
-	}).then((res: Response) => {
-		if (!res.ok) return false;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-		void res.text().then((token: string) => _handleLoginResponse(email, token));
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDLmrZHMO0e0t9fUNOyI5ixpl2j-mRNTA8',
+	authDomain: 'taskratchet-dev.firebaseapp.com',
+	databaseURL: 'https://taskratchet-dev.firebaseio.com',
+	projectId: 'taskratchet-dev',
+	storageBucket: 'taskratchet-dev.appspot.com',
+	messagingSenderId: '1073811542141',
+	appId: '1:1073811542141:web:62a74298b7a3b27cc3e114',
+};
 
+<<<<<<< Updated upstream
 		return true;
+=======
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export async function login(email: string, password: string): Promise<boolean> {
+	// const cred = await signInWithEmailAndPassword(auth, email, password);
+
+	// window.localStorage.setItem('firebase_token', await cred.user.getIdToken());
+
+	const res = await fetch1('account/login', false, 'POST', {
+		email,
+		password,
+>>>>>>> Stashed changes
 	});
+
+	if (!res.ok) return false;
+
+	void res.text().then((token: string) => _handleLoginResponse(email, token));
+
+	logEvent({
+		category: EventCategory.Authentication,
+		action: EventAction.Login,
+	});
+
+	return true;
 }
 
 function _handleLoginResponse(email: string, token: string): void {
