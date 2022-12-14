23a3d1cd146048a7df813728884bf2c6a00fc42e
Nathan Arthur <nathan@nathanarthur.com>
2022-12-14 15:14:17 -0500

    add firebase bundle chunk

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
 					manualChunks: {
 						dom: ['react-dom'],
 						material: ['@mui/material'],
+						firebase: ['firebase/auth'],
 					},
 				},
 			},
