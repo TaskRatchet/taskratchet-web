b1bd4b6807cb2751e549258124beb1fcda17d743
Nathan Arthur <nathan@nathanarthur.com>
2023-01-31 14:57:16 -0500

    fix broken test, lint issue

--- src/components/HOCs/AndTheme.tsx
+++ src/components/HOCs/AndTheme.tsx
@@ -9,7 +9,7 @@ import {
 	Link as RouterLink,
 	LinkProps as RouterLinkProps,
 } from 'react-router-dom';
-import Link, { LinkProps } from '@mui/material/Link';
+import { LinkProps } from '@mui/material/Link';
 
 const colors = {
 	warm: {
