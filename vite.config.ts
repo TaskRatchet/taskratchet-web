20c21b43d0c5e9a31277b9f98fdee2ff6462e886
Nathan Arthur <nathan@nathanarthur.com>
2024-08-05 16:26:49 -0400

    rewrite bundle.yml

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd());
 
 	const config: UserConfig = {
-		plugins: [react(), visualizer()],
+		plugins: [react(), visualizer({template: 'raw-data'})],
 		test: {
 			environment: 'jsdom',
 			setupFiles: ['./global-setup.ts'],
