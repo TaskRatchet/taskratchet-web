61a9907b3a1ebc3702b7eaccada084e52a8a8510
Nathan Arthur <nathan@nathanarthur.com>
2024-11-17 12:46:02 -0500

    logout button

---
+++ src/components/Nav.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { logout } from '@taskratchet/sdk';
+</script>
+
+<nav>
+	<a href="/">Next</a>
+	<a href="/archive">Archive</a>
+	<a
+		href="#"
+		on:click={() => {
+			logout();
+			window.location.href = '/login';
+		}}>Logout</a
+	>
+</nav>
+
+<style>
+	nav {
+		padding: 1rem;
+		display: flex;
+		gap: 1rem;
+		justify-content: center;
+	}
+
+	nav a {
+		text-decoration: none;
+	}
+
+	nav a:hover {
+		text-decoration: underline;
+	}
+</style>
