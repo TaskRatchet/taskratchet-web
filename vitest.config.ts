6778193139f56f1ab09f5cca9705fa68b127a7d3
Nathan Arthur <nathan@nathanarthur.com>
2024-11-20 13:46:43 -0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser'],
+			}
+		: undefined,
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./global-setup.ts'],
@@ -12,7 +17,7 @@ export default defineConfig({
 			'**/cypress/**',
 			'**/.{idea,git,cache,output,temp}/**',
 			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-			'**/src/react/**'
+			'**/src/react/**',
 		],
 	},
 });
