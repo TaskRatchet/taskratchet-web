79928ec18e060222e534ad6436f1d0e0d92cc8c5
Nathan Arthur <nathan@nathanarthur.com>
2025-06-27 13:20:23 -0400

    fix: remove unnecessary wrapper

--- src/react/components/organisms/FeedbackButton.tsx
+++ src/react/components/organisms/FeedbackButton.tsx
@@ -4,21 +4,19 @@ import FeedbackIcon from '@mui/icons-material/Feedback';
 
 export default function FeedbackButton() {
 	return (
-		<>
-			<Tooltip title={'Contact'}>
-				<IconButton
-					component="a"
-					href="https://docs.taskratchet.com/contact.html"
-					target="_blank"
-					rel="noopener noreferrer"
-					edge="start"
-					color="inherit"
-					aria-label="contact"
-					sx={{ m: 0.1 }}
-				>
-					<FeedbackIcon />
-				</IconButton>
-			</Tooltip>
-		</>
+		<Tooltip title={'Contact'}>
+			<IconButton
+				component="a"
+				href="https://docs.taskratchet.com/contact.html"
+				target="_blank"
+				rel="noopener noreferrer"
+				edge="start"
+				color="inherit"
+				aria-label="contact"
+				sx={{ m: 0.1 }}
+			>
+				<FeedbackIcon />
+			</IconButton>
+		</Tooltip>
 	);
 }
