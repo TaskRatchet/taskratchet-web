2ffe457d5d88c50ab7e6c6318782950ad47deec6
Nathan Arthur <nathan@nathanarthur.com>
2025-08-12 15:52:19 -0400

    remove unused stuff

--- src/lib/formatDue.ts
+++
@@ -1,9 +0,0 @@
-export default function formatDue(date: Date): string {
-	return date.toLocaleDateString('en-US', {
-		year: 'numeric',
-		month: 'numeric',
-		day: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric',
-	});
-}
