f116c820dc7a1f478996a420266a49a70395d24b
Nathan Arthur <nathan@nathanarthur.com>
2025-08-12 15:57:11 -0400

    remove unused constants

--- src/tr_constants.ts
+++ src/tr_constants.ts
@@ -1,14 +1,3 @@
-export const HOSTNAME = window && window.location && window.location.hostname;
+const HOSTNAME = window && window.location && window.location.hostname;
+
 export const IS_PRODUCTION = HOSTNAME === 'app.taskratchet.com';
-export const IS_STAGING = HOSTNAME.includes('deploy-preview');
-export const IS_LOCAL = !IS_PRODUCTION && !IS_STAGING;
-export const API1_BASE = IS_PRODUCTION
-	? 'https://api.taskratchet.com/api1/'
-	: IS_STAGING
-		? 'https://taskratchet-api-node-c3yk2gl5eq-uc.a.run.app/api1/'
-		: 'http://localhost:8080/api1/';
-export const API2_BASE = IS_PRODUCTION
-	? 'https://api.taskratchet.com/api2/'
-	: IS_STAGING
-		? 'https://taskratchet-api-node-c3yk2gl5eq-uc.a.run.app/api2/'
-		: 'http://localhost:8080/api2/';
