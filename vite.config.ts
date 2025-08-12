4e4007006aa1d052cbffc5fe16caa07641395269
Nathan Arthur <nathan@nathanarthur.com>
2025-08-12 17:10:08 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default {
-	plugins: [visualizer()],
+	plugins: [visualizer({ template: 'raw-data' })],
+	build: {
+		rollupOptions: {
+			output: {
+				chunkFileNames: 'assets/[name].[hash].chunk.js',
+				entryFileNames: 'assets/[name].[hash].js',
+				manualChunks: {
+					dom: ['react-dom'],
+					material: ['@mui/material'],
+				},
+			},
+		},
+	},
 };
