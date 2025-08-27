72bed6af4d76cc6ca71a9595dc32b242e113c1a7
Nathan Arthur <nathan@nathanarthur.com>
2025-08-27 12:21:41 -0400

    rename VITE_STRIPE_PUBLISHABLE_KEY

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_STRIPE_KEY: string;
+	readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
 	readonly VITE_CLERK_PUBLISHABLE_KEY: string;
 }
 
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export async function redirectToCheckout(sessionId: string): Promise<void> {
-	const stripe = await loadStripe(import.meta.env.VITE_STRIPE_KEY);
+	const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 	if (!stripe) {
 		throw new Error('Stripe not loaded');
