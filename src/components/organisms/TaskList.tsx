0f24ae60a7f21e662f86cf4771ef72f540c6fdab
Nathan Arthur <nathan@nathanarthur.com>
2025-08-27 15:24:13 -0400

    improve empty state contrast

--- src/components/organisms/TaskList.tsx
+++ src/components/organisms/TaskList.tsx
@@ -70,7 +70,7 @@ const TaskList = ({ lastToday, newTask }: TaskListProps): JSX.Element => {
 		<>
 			{isFetched && !tasks.length && (
 				<Box sx={{ p: 2 }}>
-					<Alert severity="info">
+					<Alert variant="filled" severity="info">
 						<AlertTitle>Nothing here!</AlertTitle>
 						Maybe add a task?
 					</Alert>
